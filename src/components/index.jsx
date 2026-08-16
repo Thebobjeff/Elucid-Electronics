9d8490f993ea7373ddd706922c9b75f5d6f30945
Devonte Edward <devon2575@gmail.com>
2026-08-16 11:15:43 -0400

    Product Card
    Added in a product Card so we can display items easily

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,4 @@ export { Header } from "./Header";
 export { Footer } from "./Footer";
 export { Wedges } from "./Wedges";
 export { Cycle } from "./Cycle";
+export { ProductCard } from "./ProductCard";
